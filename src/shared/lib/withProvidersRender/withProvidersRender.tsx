ff45aba0b759f623f85c81d544c6b5479f088fa3
kirillgilead <kirillgilead@gmail.com>
2023-04-22 14:44:33 +0300

    add router to test provider

--- src/shared/lib/withProvidersRender/withProvidersRender.tsx
+++ src/shared/lib/withProvidersRender/withProvidersRender.tsx
@@ -5,13 +5,21 @@ import { I18nextProvider } from 'react-i18next';
 import i18nTest from 'shared/configs/i18n/i18nforTest';
 import { MemoryRouter } from 'react-router-dom';
 
-export const withProvidersRender = (component: ReactElement, options?: RenderOptions) => render(
-    <MemoryRouter>
-        <I18nextProvider
-            i18n={i18nTest}
-        >
-            <ThemeProvider>{component}</ThemeProvider>
-        </I18nextProvider>
-    </MemoryRouter>,
-    { ...options },
-);
+export interface providerRenderOptions extends RenderOptions {
+    route?: string
+}
+
+export const withProvidersRender = (component: ReactElement, options?: providerRenderOptions) => {
+    const { route = '/' } = options;
+
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider
+                i18n={i18nTest}
+            >
+                <ThemeProvider>{component}</ThemeProvider>
+            </I18nextProvider>
+        </MemoryRouter>,
+        { ...options },
+    );
+};
