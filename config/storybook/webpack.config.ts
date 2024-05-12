f9a54c5d2dea7fcfac9583070a7b779452342b38
Kirill gai <kirillgilead@gamil.com>
2024-05-12 16:00:23 +0300

    storybook webpack config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,11 +11,12 @@ export default ({ config }: { config: webpack.Configuration }) => {
     src: path.resolve(__dirname, '..', '..', 'src'),
   };
 
-  config.resolve.modules.unshift(paths.src);
-  config.resolve.extensions.push('.ts', '.tsx');
+  config!.resolve!.modules!.unshift(paths.src);
+  config!.resolve!.extensions!.push('.ts', '.tsx');
 
   // eslint-disable-next-line no-param-reassign
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+  // @ts-ignore
+  config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
@@ -23,12 +24,12 @@ export default ({ config }: { config: webpack.Configuration }) => {
     return rule;
   });
 
-  config.module.rules.push({
+  config!.module!.rules!.push({
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   });
 
-  config.module.rules.push(buildCssLoader(true));
+  config!.module!.rules!.push(buildCssLoader(true));
 
   config!.plugins!.push(
     new DefinePlugin({
